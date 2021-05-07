d5d2b2a300867a1e1461f5a8e0ce95efd446d335
zhang <1826378013@qq.com>
2021-05-07 16:56:26 +0800

    add：stream filter

--- dubbo/gateway/src/main/java/com/philosopherzb/gateway/GatewayApplication.java
+++ dubbo/gateway/src/main/java/com/philosopherzb/gateway/GatewayApplication.java
@@ -2,10 +2,12 @@ package com.philosopherzb.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource(value = {"classpath:/dubbo-consumer.xml"})
+@ServletComponentScan("com.philosopherzb.gateway.filter")
 public class GatewayApplication {
 
     public static void main(String[] args) {
---
+++ dubbo/gateway/src/main/java/com/philosopherzb/gateway/filter/HttpHelper.java
@@ -0,0 +1,46 @@
+package com.philosopherzb.gateway.filter;
+
+import javax.servlet.ServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+/**
+ * @author philosopherZB
+ * @date 2021/5/6
+ */
+class HttpHelper {
+    static String getBodyString(ServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+        InputStream inputStream = null;
+        BufferedReader reader = null;
+        try {
+            inputStream = request.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
