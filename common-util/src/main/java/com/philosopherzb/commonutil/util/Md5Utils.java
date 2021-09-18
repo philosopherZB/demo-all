169fee718763f87f336ba18694ab1bbe9a6de952
zhang <1826378013@qq.com>
2021-09-18 10:58:18 +0800

    fix: Md5Utils 数组格式修正

--- common-util/src/main/java/com/philosopherzb/commonutil/util/Md5Utils.java
+++ common-util/src/main/java/com/philosopherzb/commonutil/util/Md5Utils.java
@@ -20,7 +20,7 @@ public class Md5Utils {
     /**
      * 默认的密码字符串组合，用来将字节转换成 16 进制表示的字符,apache校验下载的文件的正确性用的就是默认的这个组合
      */
-    private static final char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    private static final char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     private static MessageDigest messagedigest = null;
 
@@ -115,7 +115,7 @@ public class Md5Utils {
      * @param bytes 字节数组
      * @return 字符串
      */
-    private static String bufferToHex(byte bytes[]) {
+    private static String bufferToHex(byte[] bytes) {
         int m = 0;
         int n = bytes.length;
         StringBuffer stringbuffer = new StringBuffer(2 * n);
